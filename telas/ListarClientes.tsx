914504ea226e4542a18bd76f7646a8f79265fb8e
Thiago Silva <59424663+ithiagosilva@users.noreply.github.com>
2025-10-25 11:50:04 -0300

    criando função de listagem

---
+++ telas/ListarClientes.tsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import Cliente from "../componentes/Cliente";
+
+import api from "../componentes/Api";
+
+export default function ListarClientes() {
+  type ClienteType = { id: number; nome: string; cpf: string; saldo: number };
+
+  const [clientes, setClientes] = useState<ClienteType[]>([]);
+  async function buscarClientes() {
+    const response = await api.get("clientes");
+    setClientes(response.data);
+  }
+
+  useEffect(() => {
+    buscarClientes();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <View style={styles.bloco}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.txtBtn}>Cadastrar Novo Cliente</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.titulo}> Listagem de Clientes</Text>
+
+      <View style={styles.bloco}>
+        <FlatList
+          data={clientes}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <Cliente
+              nome={item.nome}
+              cpf={item.cpf}
+              saldo={item.saldo}
+              id={item.id}
+            />
+          )}
+          style={styles.lista}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  bloco: {
+    width: "100%",
+  },
+  btn: {
+    backgroundColor: "#669988",
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginTop: 20,
+    borderRadius: 20,
+  },
+  txtBtn: {
+    textAlign: "center",
+    fontSize: 20,
+  },
+  lista: {
+    width: "90%",
+    height: "80%",
+    marginLeft: "5%",
+  },
+});
